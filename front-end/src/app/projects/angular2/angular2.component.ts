4011aab15472f0c007c87e258cfb3be05367d53a
Dillon Woodland <54955292+dillonwoodland@users.noreply.github.com>
2024-01-19 15:07:18 -0700

    finished the second angular project page

--- front-end/src/app/projects/angular2/angular2.component.ts
+++ front-end/src/app/projects/angular2/angular2.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-angular2',
   templateUrl: './angular2.component.html',
-  styleUrls: ['./angular2.component.css']
+  styleUrls: ['../projects.css']
 })
 export class Angular2Component {
-
+  constructor(private router: Router) { }
+  backButtonClicked() {
+    this.router.navigate(['Projects']);
+  }
 }
