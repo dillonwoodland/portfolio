60f6e6337886309bd66a656b07ba8bbdadc5e621
Dillon Woodland <54955292+dillonwoodland@users.noreply.github.com>
2024-01-23 16:48:57 -0700

    Finished project page for STDM. Changed video dimensions. I plan to change these on all the project videos

--- front-end/src/app/projects/stdm/stdm.component.ts
+++ front-end/src/app/projects/stdm/stdm.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-stdm',
   templateUrl: './stdm.component.html',
-  styleUrls: ['./stdm.component.css']
+  styleUrls: ['../projects.css']
 })
 export class StdmComponent {
+  constructor(private router: Router) { }
 
+  videoLoaded: boolean = false;
+  backButtonClicked() {
+    this.router.navigate(['Projects']);
+  }
 }
