0ad7c3190fcdbd4d9d0590e6899a3b6b4cfc5fd5
Dillon Woodland <54955292+dillonwoodland@users.noreply.github.com>
2024-01-23 16:21:25 -0700

    Finished the tcp-udp project page.

--- front-end/src/app/projects/tcp-udp/tcp-udp.component.ts
+++ front-end/src/app/projects/tcp-udp/tcp-udp.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tcp-udp',
   templateUrl: './tcp-udp.component.html',
-  styleUrls: ['./tcp-udp.component.css']
+  styleUrls: ['../projects.css']
 })
 export class TcpUdpComponent {
+  constructor(private router: Router) { }
 
+  videoLoaded: boolean = false;
+  backButtonClicked() {
+    this.router.navigate(['Projects']);
+  }
 }
